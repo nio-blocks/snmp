675d8c3cec7a767f9227b08869abd14bb99d54d5
mattdodge <matt@n.io>
2015-04-15 18:08:35 -0700

    Making block signal input driven - fixes #5

--- snmp_block.py
+++ snmp_block.py
@@ -4,17 +4,20 @@
 
 """
 from enum import Enum
+from pysnmp.entity.rfc3413.oneliner import cmdgen
 from nio.common.block.attribute import Output
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
 from nio.common.signal.base import Signal
 from nio.common.signal.status import StatusSignal, SignalStatus
 from nio.metadata.properties import TimeDeltaProperty, BoolProperty, \
-    SelectProperty, ListProperty
-from nio.metadata.properties.int import IntProperty
-from nio.metadata.properties.string import StringProperty
-from nio.modules.scheduler import Job
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+    SelectProperty, ListProperty, IntProperty, StringProperty, \
+    ExpressionProperty, PropertyHolder
+from nio.metadata.properties.version import VersionProperty
+
+
+class OIDProperty(PropertyHolder):
+    oid = ExpressionProperty(title='OID', default='{{ $oid }}')
 
 
 @Output("status")
@@ -28,14 +31,13 @@ class BaseSNMPBlock(Block):
 
     agent_host = StringProperty(title="SNMP Agent Url", default='127.0.0.1')
     agent_port = IntProperty(title="SNMP Agent Port", default=161)
-    pool_interval = TimeDeltaProperty(
-        title='Pooling interval', default={"seconds": 10})
     timeout = TimeDeltaProperty(title='SNMP GET Timeout',
                                 default={"seconds": 1})
     retries = IntProperty(title="SNMP GET Retries", default=5)
     lookup_names = BoolProperty(title="Look up OId names", default=False)
     lookup_values = BoolProperty(title="Look up OId values", default=False)
-    oids = ListProperty(str, title="List of OID")
+    oids = ListProperty(OIDProperty, title="List of OID")
+    version = VersionProperty('0.2.0')
 
     def __init__(self):
         super().__init__()
@@ -51,13 +53,27 @@ class BaseSNMPBlock(Block):
         """
         raise NotImplementedError()
 
-    def _request_GET(self):
+    def process_signals(self, signals, input_id='default'):
+        for signal in signals:
+            valid_oids = []
+            for oid in self.oids:
+                try:
+                    next_oid = oid.oid(signal)
+                    valid_oids.append(next_oid)
+                except:
+                    self._logger.exception(
+                        "Could not determine OID from {}".format(oid))
+
+            if valid_oids:
+                self.execute_request(valid_oids)
+
+    def execute_request(self, oids):
         """ Executes SNMP GET request
         """
         errorIndication, errorStatus, errorIndex, varBinds = \
             self._cmdGen.getCmd(self._data,
                                 self._transport,
-                                *self.oids,
+                                *oids,
                                 lookupNames=self.lookup_names,
                                 lookupValues=self.lookup_values)
 
@@ -83,16 +99,16 @@ class BaseSNMPBlock(Block):
         """
         error = ('%s at %s' % (
             errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex)-1][0] or '?'
-            )
+            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'
+        )
         )
         self._logger.error("SNMP GET Error: %s" % error)
         self.notify_signals([Signal({"error": error})], "status")
         # also report this on the management channel as warning
         self.notify_management_signal(StatusSignal(SignalStatus.warning,
                                                    error))
-        #TODO: other values returned in other index other than errorIndex
-        #TODO: have valid values ?
+        # TODO: other values returned in other index other than errorIndex
+        # TODO: have valid values ?
 
     def _handle_data(self, varBinds):
         """ Notify signals in the "default" output
@@ -116,18 +132,6 @@ class BaseSNMPBlock(Block):
             timeout=self.timeout.seconds,
             retries=self.retries)
 
-    def start(self):
-        super().start()
-        self._job = Job(self._request_GET, self.pool_interval, True)
-        self._logger.info("SNMP polling started")
-
-    def stop(self):
-        if self._job:
-            self._job.cancel()
-            self._job = None
-        self._logger.info("SNMP polling stopped")
-        super().stop()
-
 
 class SNMPType(Enum):
     SMIv1 = 0
@@ -136,6 +140,7 @@ class SNMPType(Enum):
 
 @Discoverable(DiscoverableType.block)
 class SNMPBlock(BaseSNMPBlock):
+
     """ SNMP block supporting SNMP v1 and v2
 
     """
--- tests/test_snmp_block.py
+++ tests/test_snmp_block.py
@@ -1,6 +1,6 @@
 import datetime
-from time import sleep
 from unittest.mock import MagicMock
+from nio.common.signal.base import Signal
 from nio.common.signal.status import StatusSignal, SignalStatus
 from nio.util.support.block_test_case import NIOBlockTestCase
 from pysnmp.entity.rfc3413.oneliner.target import UdpTransportTarget
@@ -17,71 +17,57 @@ class TestSNMPBlock(NIOBlockTestCase):
         self.assertIsNone(block._transport)
         self.assertIsNone(block._job)
         self.assertIsNotNone(block._cmdGen)
-        self.assertEqual("127.0.0.1",block.agent_host)
-        self.assertEqual(161,block.agent_port)
-        self.assertEqual(datetime.timedelta(0, 1),block.timeout)
-        self.assertEqual(5,block.retries)
-        self.assertEqual(datetime.timedelta(0, 10),block.pool_interval)
+        self.assertEqual("127.0.0.1", block.agent_host)
+        self.assertEqual(161, block.agent_port)
+        self.assertEqual(datetime.timedelta(0, 1), block.timeout)
+        self.assertEqual(5, block.retries)
         self.assertFalse(block.lookup_names)
         self.assertFalse(block.lookup_names)
 
-    def test_configure(self):
-        block = SNMPBlock()
-        block._create_data = MagicMock()
+    def configure_block_with_oids(self, block, oids):
         self.configure_block(block, {
             "agent_host": "127.0.0.1",
             "agent_port": 161,
             "community": "public",
-            "oids": ["1.3.6.1.2.1.31.1.1.1.6.2"],
+            "oids": [{'oid': oid} for oid in oids],
             "lookup_names": True
         })
+
+    def test_configure(self):
+        block = SNMPBlock()
+        block._create_data = MagicMock()
+        self.configure_block_with_oids(block, ["1.3.6.1.2.1.31.1.1.1.6.2"])
         block._create_data.assert_called_once_with()
         self.assertEqual(UdpTransportTarget, block._transport.__class__)
         self.assertEqual(5, block._transport.retries)
         self.assertEqual(1, block._transport.timeout)
 
-    def test_job_called(self):
+    def test_request_made(self):
         block = SNMPBlock()
-        block._request_GET = MagicMock()
-        self.configure_block(block, {
-            "agent_host": "127.0.0.1",
-            "agent_port": 161,
-            "community": "public",
-            "pool_interval": {"seconds": 1},
-            "oids": ["1.3.6.1.2.1.31.1.1.1.6.2"],
-            "lookup_names": True
-        })
+        block.execute_request = MagicMock()
+        my_oid = ["1.3.6.1.2.1.31.1.1.1.6.2"]
+        self.configure_block_with_oids(block, my_oid)
         block.start()
-        sleep(2)
-        block._request_GET.assert_called_with()
+        block.process_signals([Signal()])
+        block.execute_request.assert_called_with(my_oid)
         block.stop()
 
     def test_request_get(self):
         block = SNMPBlock()
         block._data = MagicMock()
         block._transport = MagicMock()
-        self.configure_block(block, {
-            "agent_host": "127.0.0.1",
-            "agent_port": 161,
-            "community": "public",
-            "pool_interval": {"seconds": 1},
-            "oids": ["1.3.6.1.2.1.31.1.1.1.10.2",
-                     "1.3.6.1.2.1.31.1.1.1.6.2",
-                     "1.3.6.1.2.1.1.3.0",
-                     "1.3.6.1.4.1.12356.101.14.5.1.15",
-                     "1.3.6.1.4.1.12356.101.10.114.3.1.4"],
-            "lookup_names": True
-        })
+        my_oids = ["1.3.6.1.2.1.31.1.1.1.10.2",
+                   "1.3.6.1.2.1.31.1.1.1.6.2",
+                   "1.3.6.1.2.1.1.3.0",
+                   "1.3.6.1.4.1.12356.101.14.5.1.15",
+                   "1.3.6.1.4.1.12356.101.10.114.3.1.4"]
+        self.configure_block_with_oids(block, my_oids)
         block._cmdGen.getCmd = MagicMock(return_value=("", False, 0, []))
-        block._request_GET()
+        block.process_signals([Signal()])
         block._cmdGen.getCmd.assert_called_once_with(
             block._data,
             block._transport,
-            "1.3.6.1.2.1.31.1.1.1.10.2",
-            "1.3.6.1.2.1.31.1.1.1.6.2",
-            "1.3.6.1.2.1.1.3.0",
-            "1.3.6.1.4.1.12356.101.14.5.1.15",
-            "1.3.6.1.4.1.12356.101.10.114.3.1.4",
+            *my_oids,
             lookupNames=True, lookupValues=False)
 
     def test_error_handling(self):
@@ -89,16 +75,10 @@ class TestSNMPBlock(NIOBlockTestCase):
         block._data = MagicMock()
         block._transport = MagicMock()
         block._handle_error = MagicMock()
-        self.configure_block(block, {
-            "agent_host": "127.0.0.1",
-            "agent_port": 161,
-            "community": "public",
-            "pool_interval": {"seconds": 1},
-            "oids": ["1.3.6.1.2.1.31.1.1.1.6.2"]
-        })
+        self.configure_block_with_oids(block, ["1.3.6.1.2.1.31.1.1.1.6.2"])
         block._cmdGen.getCmd = MagicMock(
             return_value=("No response before timeout", False, 0, {}))
-        block._request_GET()
+        block.process_signals([Signal()])
         block._handle_error.assert_called_once_with(
             "No response before timeout")
 
@@ -113,16 +93,10 @@ class TestSNMPBlock(NIOBlockTestCase):
         block._data = MagicMock()
         block._transport = MagicMock()
         block.notify_management_signal = my_notify_mgmt_signal
-        self.configure_block(block, {
-            "agent_host": "127.0.0.1",
-            "agent_port": 161,
-            "community": "public",
-            "pool_interval": {"seconds": 1},
-            "oids": ["1.3.6.1.2.1.31.1.1.1.6.2"]
-        })
+        self.configure_block_with_oids(block, ["1.3.6.1.2.1.31.1.1.1.6.2"])
         block._cmdGen.getCmd = MagicMock(
             return_value=("No response before timeout", False, 0, {}))
-        block._request_GET()
+        block.process_signals([Signal()])
 
     def test_error_status_handling(self):
 
@@ -143,20 +117,14 @@ class TestSNMPBlock(NIOBlockTestCase):
         block._transport = MagicMock()
         block.notify_management_signal = my_notify_mgmt_signal
         block.notify_signals = my_handle_signal
-        self.configure_block(block, {
-            "agent_host": "127.0.0.1",
-            "agent_port": 161,
-            "community": "public",
-            "pool_interval": {"seconds": 1},
-            "oids": ["1.3.6.1.2.1.31.1.1.1.6.2"]
-        })
+        self.configure_block_with_oids(block, ["1.3.6.1.2.1.31.1.1.1.6.2"])
         block._cmdGen.getCmd = MagicMock(
             return_value=(None,
                           OctetString("low battery"),
                           1,
                           [(ObjectName("1.3.6.1.2.1.31.1.1.1.6.2"),
                             OctetString(""))]))
-        block._request_GET()
+        block.process_signals([Signal()])
 
     def test_create_data(self):
         block = BaseSNMPBlock()
