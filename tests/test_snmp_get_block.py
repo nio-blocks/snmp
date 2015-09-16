656ccd803bb8258b8a61ae1c5e907de1bb31fc6a
Chris Brackert <cbrackert@gmail.com>
2015-09-15 18:13:12 -0700

    Add unit tests for SNMPGet and SNMPWalk blocks

---
+++ tests/test_snmp_get_block.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock
+from nio.common.signal.base import Signal
+from nio.util.support.block_test_case import NIOBlockTestCase
+from ..snmp_get_block import SNMPGet
+
+
+SAMPLE_SNMP_RESPONSE = ("", False, 0, [])
+SAMPLE_ERROR_SNMP_RESPONSE = ("ERROR", False, 0, [])
+SAMPLE_ERROR_STATUS_SNMP_RESPONSE = ("", True, 123, [])
+
+
+class TestSNMPGet(NIOBlockTestCase):
+
+    def test_hard_coded_oid(self):
+        """ Make sure that hard coded OIDs are queried properly """
+        block = SNMPGet()
+        myOID = "1.3.6.1.2.1.31.1.1.1.6.2"
+        starting_signal = Signal({"existing_key": "existing_val"})
+        self.configure_block(block, {
+            "oids": [{"oid": myOID}]
+        })
+        block._cmdGen = MagicMock()
+        block.start()
+        # Send the starting signal, make sure everything was called correctly
+        block.process_signals([starting_signal])
+        args, kwargs = block._cmdGen.getCmd.call_args
+        self.assertEqual(args[2], myOID)
+        block.stop()
