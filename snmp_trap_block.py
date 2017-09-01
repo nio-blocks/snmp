e22a79aa98d30d42a27d142309610dd1826cbc35
neutralioJenkins <jenkins@n.io>
2017-09-01 15:37:55 -0600

    Bump version to v0.4.0

--- snmp_trap_block.py
+++ snmp_trap_block.py
@@ -17,7 +17,7 @@ class SNMPTrap(Block):
 
     ip_address = StringProperty(title='IP Address', default='127.0.0.1')
     port = IntProperty(title='Port', default=162)
-    version = VersionProperty('0.4.0')
+    version = VersionProperty("0.4.0")
 
     def __init__(self):
         super().__init__()
